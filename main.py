924dca9ea758e944700c165b6007a0ed711d52e2
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-01-08 13:55:40 +0700

    add vertical line func with detail

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from kivy.app import App 
 from kivy.properties import NumericProperty
 from kivy.uix.widget import Widget
-
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Line
 
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     
-    def __int__(self, **kwargs):
+    def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print("INIT W:" + str(self.width) + " H:" + str(self.height))
+        self.init_vertical_line()
     
     def on_parent(self, widget, parent):
         # print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
@@ -28,6 +30,11 @@ class MainWidget(Widget):
     def on_perspective_point_y(self, widget, value):
         # print("PY: " + str(value))
         pass
+    
+    def init_vertical_line(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            Line(points=[100, 0, 100, 100])
     
     
 class GalaxyApp(App):
