642775976ed1922dadc3eacc6065eb49116fee05
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-01-08 14:07:21 +0700

    detail vertical line

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     
-    line = None
+    V_NB_LINES = 7
+    V_LINES_SPACING = .1 # percentage in screen width
+    vertical_lines = []
     
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -23,7 +25,7 @@ class MainWidget(Widget):
         # print("ON SIZE W:" + str(self.width) + " H:" + str(self.height))
         # self.perspective_point_x = self.width/2
         # self.perspective_point_y = self.height * 0.75
-        pass
+        self.update_vertical_lines()
     
     def on_perspective_point_x(self, widget, value):
         # print("PX: " + str(value))
@@ -36,10 +38,18 @@ class MainWidget(Widget):
     def init_vertical_line(self):
         with self.canvas:
             Color(1, 1, 1)
-            self.line = Line(points=[self.width/2, 0, self.width/2, 100])
+            # self.line = Line(points=[100, 0, 200, 100])
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
     
     def update_vertical_lines(self):
-        self.line.points = []
+        central_line_x = int(self.width / 2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2 )  
+        for i in range(0, self.V_NB_LINES):
+            line_x = central_line_x + offset*spacing
+            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+            offset += 1
     
     
 class GalaxyApp(App):
