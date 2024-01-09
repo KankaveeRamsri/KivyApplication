6bad125db58a30040ef4eb1029f2d3b8239dc9a2
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-01-09 15:45:44 +0700

    horizontal lines perspective

--- main.py
+++ main.py
@@ -90,16 +90,18 @@ class MainWidget(Widget):
         return int(x), int(y)
     
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
             
         
         diff_x = x-self.perspective_point_x
-        diff_y = self.perspective_point_y-tr_y
-        proportion_y = diff_y/self.perspective_point_y # 1 when diff_y == self.perspective_point_y / 0 when diff_y = 0 
+        diff_y = self.perspective_point_y-lin_y
+        factor_y = diff_y/self.perspective_point_y # 1 when diff_y == self.perspective_point_y / 0 when diff_y = 0 
+        factor_y = factor_y * factor_y
         
-        tr_x = self.perspective_point_x + diff_x*proportion_y
+        tr_x = self.perspective_point_x + diff_x*factor_y
+        tr_y = self.perspective_point_y - factor_y*self.perspective_point_y
         
         return int(tr_x), int(tr_y)
     
