c4e4638b26d8f8bc9a854415396d19895b86374d
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-01-10 21:35:35 +0700

    built transform file

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from kivy.properties import Clock
 
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2D, transform_perspective
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     
@@ -116,30 +117,6 @@ class MainWidget(Widget):
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
-            
-    
-    def transform(self, x, y):
-        # return self.transform_2D(x, y)
-        return self.transform_perspective(x, y) 
-    
-    def transform_2D(self, x, y):
-        return int(x), int(y)
-    
-    def transform_perspective(self, x, y):
-        lin_y = y * self.perspective_point_y / self.height
-        if lin_y > self.perspective_point_y:
-            lin_y = self.perspective_point_y
-            
-        
-        diff_x = x-self.perspective_point_x
-        diff_y = self.perspective_point_y-lin_y
-        factor_y = diff_y/self.perspective_point_y # 1 when diff_y == self.perspective_point_y / 0 when diff_y = 0 
-        factor_y = pow(factor_y,4) 
-        
-        tr_x = self.perspective_point_x + diff_x*factor_y
-        tr_y = self.perspective_point_y - factor_y*self.perspective_point_y
-        
-        return int(tr_x), int(tr_y)
     
     def on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'left':
