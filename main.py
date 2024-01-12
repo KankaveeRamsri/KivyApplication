4281a297149ae4fe018a7c343d24791624e1eaa6
Kankawee Ramsri <email kankaveeramsri22@gmail.com>
2024-01-12 15:34:19 +0700

    infinite generator

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class MainWidget(Widget):
     current_speed_x = 0
     current_offset_x = 0
     
-    NB_TILES = 8
+    NB_TILES = 4
     tiles = []
     tiles_coordinates = []
     
@@ -66,9 +66,25 @@ class MainWidget(Widget):
             for i in range(0, self.NB_TILES):
                 self.tiles.append(Quad())
     
-    def generate_tiles_coordinates(self):
-        for i in range(0, self.NB_TILES):
-            self.tiles_coordinates.append((0, i))
+    def generate_tiles_coordinates(self): 
+        last_y = 0
+        
+        # clean the coordinates that are out of the screen
+        # tr_y < self.current_y_loop
+        for i in range(len(self.tiles_coordinates)-1, -1 ,-1):
+            if self.tiles_coordinates[i][1] < self.current_y_loop:
+                del self.tiles_coordinates[i]
+        
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_y = last_coordinates[1] + 1
+        print("foo1")
+        
+        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+            self.tiles_coordinates.append((0, last_y))
+            last_y += 1
+            
+        print("foo2")
            
     def init_vertical_lines(self):
         with self.canvas:
@@ -153,6 +169,7 @@ class MainWidget(Widget):
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
             self.current_y_loop += 1
+            self.generate_tiles_coordinates()
             print("loop : " + str(self.current_y_loop))
             
         # self.current_offset_x += self.current_speed_x * time_factor
